80be93d897a7b685f3ba2aff8d8f509a965059ac
YuGeonHui <ghyu_0906@naver.com>
2022-04-17 19:03:26 +0900

    Test Commit

--- SwiftAgorithmNote/Programmars/Level1/addDigits.swift
+++ SwiftAgorithmNote/Programmars/Level1/addDigits.swift
@@ -8,3 +8,12 @@
 import Foundation
 
 // MARK: - 자릿 수 더하기
+func solution(_ n: Int) -> Int
+{
+    var answer:Int = 0
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
+    
+    return answer
+}
