69e852b992be5e92724912b1fc178a8d03a29533
YuGeonHui <ghyu_0906@naver.com>
2022-04-27 22:46:07 +0900

    Test

---
+++ SwiftAgorithmNote/Programmars/Level1/pyCount.swift
@@ -0,0 +1,19 @@
+//
+//  pyCount.swift
+//  SwiftAgorithmNote
+//
+//  Created by geonhui Yu on 2022/04/27.
+//
+
+import Foundation
+
+func solution(_ s: String) -> Bool
+{
+    
+    var ans: Bool = false
+    let answer = s.lowercased()
+
+    ans = ((answer.filter { $0 == "p" }.count) == (answer.filter { $0 == "y" }.count)) ? true : false
+
+    return ans
+}
