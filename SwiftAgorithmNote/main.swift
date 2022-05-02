0b0b0e6beff412050fe836aa90caea5e5d253ba0
YuGeonHui <ghyu_0906@naver.com>
2022-05-03 01:01:48 +0900

    commit Test

--- SwiftAgorithmNote/main.swift
+++ SwiftAgorithmNote/main.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 
-func solution(_ strings:[String], _ n:Int) -> [String] {
-    var result:[String] = []
+func solution(_ strings: [String], _ n: Int) -> [String] {
     
-    result = strings.sorted{
+    var result: [String] = []
+    
+    result = strings.sorted {
         // 비교하는 두 요소의 인덱스의 문자가 같을 경우 그냥 두 전체 요소를 비교하여 정렬
         Array($0)[n] == Array($1)[n] ? $0 < $1 :
         // 그렇지 않을 경우 해당 인덱스의 문자값으로만 비교하여 정렬
