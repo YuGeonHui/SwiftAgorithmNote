3c0188bc27889a6d210a18240259b1b07fbdd4fa
YuGeonHui <ghyu_0906@naver.com>
2022-05-09 00:01:46 +0900

    직사각형 별찍기

---
+++ SwiftAgorithmNote/Programmars/Level1/RectangleStar.swift
@@ -0,0 +1,16 @@
+//
+//  RectangleStar.swift
+//  SwiftAgorithmNote
+//
+//  Created by geonhui Yu on 2022/05/08.
+//
+
+import Foundation
+
+// MARK: - 직사각형 별찍기
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a, b) = (n[0], n[1])
+//
+//for _ in 1...b {
+//    print(String(repeating: "*", count: a))
+//}
--- SwiftAgorithmNote/main.swift
+++ SwiftAgorithmNote/main.swift
@@ -7,18 +7,25 @@
 
 import Foundation
 
-func solution(_ strings: [String], _ n: Int) -> [String] {
-    
-    var result: [String] = []
-    
-    result = strings.sorted {
-        // 비교하는 두 요소의 인덱스의 문자가 같을 경우 그냥 두 전체 요소를 비교하여 정렬
-        Array($0)[n] == Array($1)[n] ? $0 < $1 :
-        // 그렇지 않을 경우 해당 인덱스의 문자값으로만 비교하여 정렬
-        Array($0)[n] < Array($1)[n]
-    }
-    
-    // 최종 정렬된 배열을 반환
-    return result
-}
+//func solution(_ strings: [String], _ n: Int) -> [String] {
+//
+//    var result: [String] = []
+//
+//    result = strings.sorted {
+//        // 비교하는 두 요소의 인덱스의 문자가 같을 경우 그냥 두 전체 요소를 비교하여 정렬
+//        Array($0)[n] == Array($1)[n] ? $0 < $1 :
+//        // 그렇지 않을 경우 해당 인덱스의 문자값으로만 비교하여 정렬
+//        Array($0)[n] < Array($1)[n]
+//    }
+//
+//    // 최종 정렬된 배열을 반환
+//    return result
+//}
 
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for _ in 1...b {
+    print(String(repeating: "*", count: a))
+}
