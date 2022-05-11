ee256c2bddefbf9080a909855f03a0cc3ad94d83
YuGeonHui <ghyu_0906@naver.com>
2022-05-11 20:28:17 +0900

    직사각형 별찍기

--- SwiftAgorithmNote/Programmars/Level1/RectangleStar.swift
+++ SwiftAgorithmNote/Programmars/Level1/RectangleStar.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 // MARK: - 직사각형 별찍기
-//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
-//let (a, b) = (n[0], n[1])
-//
-//for _ in 1...b {
-//    print(String(repeating: "*", count: a))
-//}
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for _ in 1...b {
+    print(String(repeating: "*", count: a))
+}
