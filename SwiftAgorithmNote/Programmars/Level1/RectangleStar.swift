7884bd20c34ba37a6321487f67abe2be2a2588c4
YuGeonHui <ghyu_0906@naver.com>
2022-05-11 20:28:40 +0900

    오류수정

--- SwiftAgorithmNote/Programmars/Level1/RectangleStar.swift
+++ SwiftAgorithmNote/Programmars/Level1/RectangleStar.swift
@@ -8,9 +8,12 @@
 import Foundation
 
 // MARK: - 직사각형 별찍기
-let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
-let (a, b) = (n[0], n[1])
+func starRectangle() {
+    let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+    let (a, b) = (n[0], n[1])
 
-for _ in 1...b {
-    print(String(repeating: "*", count: a))
+    for _ in 1...b {
+        print(String(repeating: "*", count: a))
+    }
 }
+
