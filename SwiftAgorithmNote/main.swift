c82a708f6d4b0c11e5e76815589ae6439e6f517a
YuGeonHui <ghyu_0906@naver.com>
2022-06-15 21:06:15 +0900

    main 수정

--- SwiftAgorithmNote/main.swift
+++ SwiftAgorithmNote/main.swift
@@ -5,12 +5,12 @@
 //  Created by geonhui Yu on 2022/04/14.
 //
 
-import Foundation
-
-func solution(_ arr: [Int]) -> [Int] {
-    
-    var array = arr
-    array.remove(at: array.firstIndex(of: array.min()!)!)
-       
-    return array.count > 0 ? array : [-1]
-}
+//import Foundation
+//
+//func solution(_ arr: [Int]) -> [Int] {
+//    
+//    var array = arr
+//    array.remove(at: array.firstIndex(of: array.min()!)!)
+//       
+//    return array.count > 0 ? array : [-1]
+//}
