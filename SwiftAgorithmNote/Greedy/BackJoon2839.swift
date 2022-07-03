83f236413bb68c64c880b68bfa4b2372a350d040
YuGeonHui <ghyu_0906@naver.com>
2022-07-03 22:54:18 +0900

    체육복 빌려주기

--- SwiftAgorithmNote/Greedy/BackJoon2839.swift
+++ SwiftAgorithmNote/Greedy/BackJoon2839.swift
@@ -12,18 +12,17 @@ func solution() -> Int {
     var n = Int(readLine()!)!
     var result = 0
     
-    if n % 5 == 0 {
-        return n
-    }
+    // 나머지가 0이면 그 값이 최소
+    if n % 5 == 0 { return n }
     
-    while (n >= 0){
+    // 어떤 숫자에서 3을 계속 빼다가 그 숫자가 5의 배수가 되면 최소로 옮길 수 있다.
+    while (n >= 0) {
+        
         n -= 3
         result += 1
-        if n % 5 == 0{
-            return result + (n / 5)
-        }
+        
+        if n % 5 == 0 { return result + (n / 5) }
     }
-    
     
     return -1
 }
